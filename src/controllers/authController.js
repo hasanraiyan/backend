f1175be516c130e4e94097adcf67ad7cfbd5fd80
Raiyan Hasan <raiyanhasan2006@gmail.com>
2025-05-16 06:31:41 +0530

    feat(auth): add user authentication with JWT token generation
    Implement user registration and login functionality with JWT token generation. The `authController` handles user authentication, while `jwtUtils` provides token generation logic. This enables secure user sessions and access control.

---
+++ src/controllers/authController.js
@@ -0,0 +1,48 @@
+import User from "../models/User.model.js";
+
+import generateToken from "../utils/jwtUtils.js";
+
+const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        const user = await User.create({ name, email, password });
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        const isMatch = await user.matchPassword(password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export { registerUser, loginUser };
